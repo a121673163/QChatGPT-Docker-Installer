1f1f2ec7f5ca4aa2869d53a0e72bd44d71b91886
a121673163 <60130403+a121673163@users.noreply.github.com>
2023-02-17 19:57:07 +0800

    Update loadFile.sh

--- loadFile.sh
+++ loadFile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/RockChinQ/QChatGPT
+git clone https://github.com/a121673163/QChatGPT
 mv QChatGPT bot
 wget -O mcl.zip https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.2/mcl-2.1.2.zip
 unzip -o -d  ./mirai mcl.zip 
